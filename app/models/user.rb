678ffb89a5749109452665818c3a3b24979732c6
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-15 21:10:27 -0500

    add has secure password and validates email/password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :products
     has_many :reviewed_products, through: :reviews, source: :product
+
+    has_secure_password
+    validates :password, :email, presence: true
+    validates :email, uniqueness: true 
+
+    
 end
