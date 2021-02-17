bec2474bc66f96a051f01a4f04f01ff692d38619
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-16 22:35:42 -0500

    add seed data for donuts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Donut.create([
+    {name: "Boston Creme", color: "Brown", location: "Bay Shore"},
+    {name: "Strawberry OG", color: "Pink", location: "Mattituck"},
+    {name: "Vanilla OG", color: "White and brown", location: "Bay Shore"}   
+])
