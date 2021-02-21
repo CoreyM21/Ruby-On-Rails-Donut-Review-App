788063a632dd10aa2fc01e4f3529f250010012d0
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-21 14:23:09 -0500

    add strong params and refactor update and create methods

--- app/controllers/concerns/donuts_controller.rb
+++ app/controllers/concerns/donuts_controller.rb
@@ -12,18 +12,25 @@ class DonutsController < ApplicationController
     end
 
     def create
-        @donut= Donut.new
-        @donut.name = params[:name]
-        @donut.color = params[:color]
-        @donut.location = params[:location]
+        @donut= Donut.new(donut_params(:name, :color, :loction))
         @donut.save
         redirect_to donut_path(@donut)
       end
 
       def edit
+        @donut = Donut.find(params[:id])
       end
 
       def update
+        @donut = Donut.find(params[:id])
+        @donut.update(donut_params(:name, :color, :location))
+        redirect_to donut_path(@donut)
+      end
+
+      private
+
+      def donut_params(*args)
+        params.require(:donut).permit(*args)
       end
 
 end
