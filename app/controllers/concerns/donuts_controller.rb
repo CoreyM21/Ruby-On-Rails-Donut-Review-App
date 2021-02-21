b10f3eaf13c8d505cb9d4704619c2b77f74bfa9c
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-21 15:35:42 -0500

    add logic for update and create actions

--- app/controllers/concerns/donuts_controller.rb
+++ app/controllers/concerns/donuts_controller.rb
@@ -1,36 +1,46 @@
 class DonutsController < ApplicationController
 
+    before_action :set_donut, only: [:show, :update, :edit]
+
     def index
         @donuts = Donut.all 
     end
 
     def show
-        @donut = Donut.find(params[:id])
     end
 
     def new
+        @donut = Donut.new 
     end
 
     def create
         @donut= Donut.new(donut_params(:name, :color, :loction))
-        @donut.save
-        redirect_to donut_path(@donut)
+            if @donut.save
+            redirect_to donut_path(@donut)
+        else 
+            render :new     
+        end
       end
 
       def edit
-        @donut = Donut.find(params[:id])
       end
 
       def update
-        @donut = Donut.find(params[:id])
-        @donut.update(donut_params(:name, :color, :location))
-        redirect_to donut_path(@donut)
+        if @donut.update(donut_params(:name, :color, :location))
+            redirect_to donut_path(@donut)
+        else
+            render :edit
+        end
       end
 
       private
 
       def donut_params(*args)
         params.require(:donut).permit(*args)
+      end
+
+      def set_donut 
+        @donut = Donut.find(params[:id])
       end
 
 end
