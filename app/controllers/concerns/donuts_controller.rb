810b8a5b5be4d23b7cc98867f57965af19f1daf2
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-21 13:57:40 -0500

    add new, create, edit and update methonds

--- app/controllers/concerns/donuts_controller.rb
+++ app/controllers/concerns/donuts_controller.rb
@@ -8,4 +8,22 @@ class DonutsController < ApplicationController
         @donut = Donut.find(params[:id])
     end
 
+    def new
+    end
+
+    def create
+        @donut= Donut.new
+        @donut.name = params[:name]
+        @donut.color = params[:color]
+        @donut.location = params[:location]
+        @donut.save
+        redirect_to donut_path(@donut)
+      end
+
+      def edit
+      end
+
+      def update
+      end
+
 end
