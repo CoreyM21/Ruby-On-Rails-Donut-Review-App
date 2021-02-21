6b4c65299e6c54fd08be6a9df2405e880fa3ed53
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-21 15:36:38 -0500

    add validations for donut params

--- app/models/donut.rb
+++ app/models/donut.rb
@@ -1,5 +1,9 @@
 class Donut < ApplicationRecord
 
+    validates :name, presence: true
+    validates :color, presence: true 
+    validates :location, presence: true 
+
     
     def name_and_color
         "#{self.name} - #{self.color}"
