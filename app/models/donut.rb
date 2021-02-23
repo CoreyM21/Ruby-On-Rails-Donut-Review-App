fce0e999a0eb1a360f71eb56d6f244c0aa7c44a0
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-22 21:13:15 -0500

    add has many reveiws and has many users through reviews

--- app/models/donut.rb
+++ app/models/donut.rb
@@ -1,5 +1,8 @@
 class Donut < ApplicationRecord
 
+    has many :reviews
+    has many :users, through: reviews
+
     validates :name, presence: true
     validates :color, presence: true 
     validates :location, presence: true 
