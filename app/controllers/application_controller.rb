760d22fc5dcaa4ef18966cfa2df020d9a2d1d180
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-22 22:17:53 -0500

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+    helper_method :logged_in?
+    helper_method :authorized 
+    helper_method :authorized_to_edit
+    
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        if !logged_in?
+            flash[:message] = "Please login to view that page."
+            redirect_to login_path
+        end
+    end
+
+    def check_if_logged_in
+        redirect_to user_path(current_user) if logged_in?
+    end
+
 end
