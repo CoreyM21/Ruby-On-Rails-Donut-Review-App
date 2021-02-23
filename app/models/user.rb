2506d94c79172938da1c2c95e98292b4e9dd0a9a
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-22 22:21:36 -0500

    add link to create new donuts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :donuts #that they created
     has_many :reviewed_donuts, through: :reviews, source: :donuts 
 
-    # has_secure_password
+    has_secure_password
     validates :password, :email, presence: true
     validates :email, uniqueness: true 
     validates :password, confirmation: { case_sensitive: true }
