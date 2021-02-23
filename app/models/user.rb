25323aec547d2585be32b0260b3f2cb5fc645deb
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-22 21:45:02 -0500

    add has secure password and validations for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has many :reviews #that they created
     has many :donuts #that they created
     has_many :reviewed_donuts, through: :reviews, source: :donuts 
+
+    has_secure_password
+    validates :password, :email, presence: true
+    validates :email, uniqueness: true 
+    validates :password, confirmation: { case_sensitive: true }
 end
