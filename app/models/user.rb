030830458d9df7051fde3f5cbbd324ca3d3996c7
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-22 22:04:20 -0500

    comment out lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-    has many :reviews #that they created
-    has many :donuts #that they created
+    has_many :reviews #that they created
+    has_many :donuts #that they created
     has_many :reviewed_donuts, through: :reviews, source: :donuts 
 
-    has_secure_password
+    # has_secure_password
     validates :password, :email, presence: true
     validates :email, uniqueness: true 
     validates :password, confirmation: { case_sensitive: true }
+
+
 end
