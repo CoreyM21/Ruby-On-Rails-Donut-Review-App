04515dd8d9d8336f8fd98a1d915f9cb7bfbce200
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-22 21:38:35 -0500

    add show, new, and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+    def show
+    end
+
+    def new
+        @user = User.new 
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            # flash[:message] = "Account Created Successfully"
+            redirect_to user_path(@user)
+        else
+            @errors = @user.errors
+            render :new
+        end
+    end
 end
