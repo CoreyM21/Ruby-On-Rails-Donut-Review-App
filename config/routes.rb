74e8ea0bb193743d437739b89b22a8ae3ba8f8f1
CoreyM21 <coreyjmartin2@gmail.com>
2021-02-22 23:48:45 -0500

    fix routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
   # get '/donuts', to: 'donuts#index', as: 'donut'
   # get '/donuts/:id' to: 'donuts#show', as: 'donut'
 
-  get 'signup' => 'users#new'
-  post 'signup' => 'users#create'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
 
   resources :donuts 
   resources :reviews
